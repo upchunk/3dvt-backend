03062a9f6ad1f933cffbb791244877f6a20c1ca1
Habibul Rahman Qalbi <habibul.rahman@lndata.com>
2022-10-20 20:49:06 +0700

    feat: :sparkles: update Exception Handler

--- backend/api/exceptions.py
+++ backend/api/exceptions.py
@@ -14,28 +14,27 @@ def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
     if isinstance(exc, Throttled):
-        custom_response_data = {
-            "message": "Concurrency Limit Exceeded, Consider to Upgrade your Plan"
-        }
+        custom_response_data = {"message": exc.detail}
         response.data = custom_response_data
 
     elif isinstance(exc, AuthenticationFailed) or isinstance(exc, NotAuthenticated):
-        custom_response_data = {"message": "Wrong authentication credentials"}
+        custom_response_data = {"message": exc.detail}
         response.data = custom_response_data
 
     elif isinstance(exc, MethodNotAllowed):
-        custom_response_data = {"message": "Method not allowed"}
+        custom_response_data = {"message": exc.detail}
         response.data = custom_response_data
 
     elif isinstance(exc, ParseError):
         custom_response_data = {"message": exc.detail}
         response.data = custom_response_data
+
     elif isinstance(exc, PermissionDenied):
         custom_response_data = {
             "message": "You do not have permission to access this resource"
         }
         response.data = custom_response_data
-    # not restframework's exception
+
     elif isinstance(exc, Exception) and getattr(exc, "status_code", None) is None:
         response = Response(
             {"message": "Internal Server Error", "trace": str(exc)}, status=500
